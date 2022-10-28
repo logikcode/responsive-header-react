6efccb352386f240a9be83f47cafbb0771df61ac
Emmanuel Oladipupo <manueltech401@gmail.com>
2022-10-28 18:46:33 +0100

    added navbar responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, {useState}from "react";
 import { Container,Nav, Logo, Hamburger, MenuLink, Menu } from "./NavbarStyled";
 
+
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+
     return(
 
         <Nav>
             <Logo href="" ><span>logikcode</span></Logo>
-            <Hamburger>
+            <Hamburger onClick={()=>{setIsOpen(!isOpen)}}>
+            
                 <span></span>
                 <span></span>
                 <span></span>
             </Hamburger>
-            <Menu>
+            <Menu open={isOpen}>
                 <MenuLink>Our Work</MenuLink>
                 <MenuLink>Career</MenuLink>
                 <MenuLink>About</MenuLink>
--- src/components/NavbarStyled.js
+++ src/components/NavbarStyled.js
@@ -23,14 +23,59 @@ color: #7b7fda;
 text-decoration: none;
 font-weight: 800;
 font-size: 1.7rem;
+
+span{
+    font-weight: 300;
+    font-size: 1.3rem;
+
+}
 `
 
 export const Hamburger = styled.div`
+display: none;
+cursor: pointer;
+flex-direction: column;
+ 
+ span{
+    height: 2px;
+    width: 25px;
+    background: #767fda;
+    margin-bottom: 4px;
+    border-radius: 5px;
 
+ }
+
+ @media screen and (max-width: 768px){
+    display: flex;
+ }
 `
 export const MenuLink = styled.a`
+padding: 1rem 2rem;
+cursor: pointer;
+text-align: center;
+text-decoration: none;
+color: #67bc98;
+transition: all 0.3s ease-in;
+font-size: 0.9rem;
 
+&:hover{
+   color: #7b7fda;
+}
 `
 export const Menu = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+position: relative;
+
+
+
+@media screen and (max-width: 768px){
+    overflow: hidden;
+    flex-direction: column;
+    width: 100%;
+    height: ${({open})=>open? "300px":"0"};
+    transition: max-height 0.3s ease-in;
+}
 
 `
